53f1f7a3408486ba6ec15b306e5a5b4a0c6b07fb
Enrico Renna <erysco@gmail.com>
2021-06-25 23:27:12 +0200

    Copper Chest has now an inventory.

--- src/main/java/io/github/cyberanner/ironchests/blocks/blockentities/CopperChestBlockEntity.java
+++ src/main/java/io/github/cyberanner/ironchests/blocks/blockentities/CopperChestBlockEntity.java
@@ -1,36 +1,42 @@
 package io.github.cyberanner.ironchests.blocks.blockentities;
 
 
+import io.github.cyberanner.ironchests.implementations.ImplementedInventory;
 import io.github.cyberanner.ironchests.registry.ModBlockEntityType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 
-public class CopperChestBlockEntity extends BlockEntity {
+public class CopperChestBlockEntity extends BlockEntity implements ImplementedInventory {
 
-    // Store the current value of the number
-    private int number = 7;
+    private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(2, ItemStack.EMPTY);
 
     public CopperChestBlockEntity(BlockPos pos, BlockState state) {
-        super((ModBlockEntityType.COPPER_CHEST_ENTITY), pos, state);
+        super(ModBlockEntityType.COPPER_CHEST_ENTITY, pos, state);
     }
 
-    // Serialize the BlockEntity
     @Override
-    public NbtCompound writeNbt(NbtCompound tag) {
-        super.writeNbt(tag);
-
-        // SAve the current value of the number to the tag
-        tag.putInt("number", number);
-
-        return tag;
+    public DefaultedList<ItemStack> getItems() {
+        return inventory;
     }
 
     // Deserialize the BlockEntity
     @Override
     public void readNbt(NbtCompound tag) {
         super.readNbt(tag);
-        number = tag.getInt("number");
+        Inventories.readNbt(tag, this.inventory);
     }
+
+    // Serialize the BlockEntity
+    @Override
+    public NbtCompound writeNbt(NbtCompound tag) {
+        super.writeNbt(tag);
+        Inventories.writeNbt(tag, this.inventory);
+        return tag;
+    }
+
 }
