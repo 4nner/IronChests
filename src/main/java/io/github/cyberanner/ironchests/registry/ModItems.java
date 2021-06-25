1b71c6a3a7659e93d8b3714f05c5c78774778f27
Enrico Renna <erysco@gmail.com>
2021-06-25 14:49:40 +0200

    Implemented Creative Tab

--- src/main/java/io/github/cyberanner/ironchests/registry/ModItems.java
+++ src/main/java/io/github/cyberanner/ironchests/registry/ModItems.java
@@ -10,10 +10,10 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
 
     // Items
-    public static final Item WOOD_COPPER_UPGRADE = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Item WOOD_COPPER_UPGRADE = new Item(new Item.Settings().group(IronChests.TAB));
 
     // Block Items
-    public static final BlockItem COPPER_CHEST = new BlockItem(ModBlocks.COPPER_CHEST, new Item.Settings().group(ItemGroup.DECORATIONS));
+    public static final BlockItem COPPER_CHEST = new BlockItem(ModBlocks.COPPER_CHEST, new Item.Settings().group(IronChests.TAB));
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(IronChests.MOD_ID, "wood_copper_upgrade"), WOOD_COPPER_UPGRADE);
