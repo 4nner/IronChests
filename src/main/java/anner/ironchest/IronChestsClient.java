914531baa126eb4d8e2412b3292859903618a7bc
Myster7494 <jackychiu0101@gmail.com>
2024-09-25 23:11:31 +0800

    refactor: change payload name

--- src/main/java/anner/ironchest/IronChestsClient.java
+++ src/main/java/anner/ironchest/IronChestsClient.java
@@ -2,6 +2,7 @@ package anner.ironchest;
 
 import anner.ironchest.blocks.blockentities.CrystalChestEntity;
 import anner.ironchest.registry.ModBlockEntityRenderer;
+import anner.ironchest.registry.ModPayloads;
 import anner.ironchest.registry.ModScreenHandlers;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
@@ -22,7 +23,7 @@ public class IronChestsClient implements ClientModInitializer {
         ModBlockEntityRenderer.registerBlockEntityRenderer();
 
         // Crystal Chest Rendering
-        ClientPlayNetworking.registerGlobalReceiver(IronChests.UpdateInventory.UPDATE_INV_PACKET_ID, (payload, context) -> {
+        ClientPlayNetworking.registerGlobalReceiver(ModPayloads.CrystalChestBlockPayload.ID, (payload, context) -> {
             BlockPos pos = payload.blockPos();
             DefaultedList<ItemStack> inv = DefaultedList.ofSize(12, ItemStack.EMPTY);
             for (int i = 0; i < 12; i++) {
